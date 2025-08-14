010a23ab055a172c2bc60816afd80e3401c953dd
SHEENMON-GHO <sheenmon@gho.care>
2025-08-14 14:58:50 +0530

    change settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -8,7 +8,8 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'url-shortner-trah.onrender.com']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
