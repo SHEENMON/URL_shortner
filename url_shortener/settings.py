50f91fa2863faaeb1a3577ab34d1a9c4f2ce6417
SHEENMON-GHO <sheenmon@gho.care>
2025-08-14 14:41:25 +0530

    Initial commit with Django project setup and Render database config

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -53,7 +53,7 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
